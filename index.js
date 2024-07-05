b7a8001785efdadb7270a56fb431e2ef719b0534
Aurosish Rout <kishanaurosish@gmail.com>
2024-07-06 02:34:46 +0530

    violate click addded

--- index.js
+++ index.js
@@ -1,24 +1,69 @@
+import { timeout } from 'puppeteer';
 import { launch } from 'puppeteer';
 
 async function scrap() {
     let browser = await launch({ headless: false });
     let page = await browser.newPage();
 
-    await page.goto("https://tirangaapk.com/#/login", { waitUntil:'domcontentloaded' });
+    await page.goto("https://tirangaapk.com/#/login", { waitUntil: 'networkidle0', timeout: 60000 });
 
     await page.waitForSelector('.phoneInput__container-input input[name="userNumber"]');
     await page.waitForSelector('.passwordInput__container-input input[type="password"]');
     await page.waitForSelector('.signIn__container-button button:nth-child(1)');
 
-
-
-
-    await page.type('.phoneInput__container-input input[name="userNumber"]', '1294567890');
-
-    await page.type('.passwordInput__container-input input[type="password"]', 'wdokjnwdwod');
+    await page.type('.phoneInput__container-input input[name="userNumber"]', '6396048902');
+    await page.type('.passwordInput__container-input input[type="password"]', 'asad2005');
 
     await page.click('.signIn__container-button button:nth-child(1)');
 
+    await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+    // Adding a delay to ensure everything is loaded
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+
+    const confirmButtonSelector = '.van-button__text';
+    const confirmButton = await page.$(confirmButtonSelector);
+
+    if (confirmButton) {
+        await confirmButton.click();
+    } else {
+        console.log("Confirm button not found");
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    await page.waitForSelector('.daman-lottery');
+
+    const firstChildSelector = '.daman-lottery > .daman_img:first-child';
+
+    const firstChild = await page.$(firstChildSelector);
+
+    if (firstChild) {
+
+        const buttonSelector = '.daman-btn';
+        const button = await firstChild.$(buttonSelector);
+        if (button) {
+            console.log("Button found, clicking...");
+            await button.click();
+        } else {
+            console.log("Button not found inside the first child");
+        }
+    } else {
+        console.log("First child not found inside .damn-lottery");
+    }
+
+
+    // let violate = 'Betting__C-head Betting__C-head-p';
+
+
+    // await page.waitForSelector(violate);
+
+    // if(violate){
+    //     violate.click();
+    // }
+
+    // await browser.close();
 }
 
 scrap().catch(error => console.error('Error:', error));
