bd67b61e94fefb4b18d8e81d608fe69b2fe19090
Aurosish Rout <kishanaurosish@gmail.com>
2024-07-10 09:07:55 +0530

    broswer installation 3

--- install-chrome.sh
+++ install-chrome.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install necessary packages and Chrome
-sudo apt-get update
-sudo apt-get install -y wget gnupg
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+apt-get update
+apt-get install -y wget gnupg
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get update
+apt-get install -y google-chrome-stable
