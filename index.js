83662ea49e560122987c9c1617497f8d3ecda883
Aurosish Rout <kishanaurosish@gmail.com>
2024-07-21 15:42:30 +0530

    trying deploy with docker 1.2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let browser;
 
 dotnv.config();
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
