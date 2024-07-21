bafcc3af29fbb8a2fd10f9f721de1ac49797565e
Aurosish Rout <kishanaurosish@gmail.com>
2024-07-21 12:25:34 +0530

    trying deploy with x..b3

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install Xvfb and xauth
-apt-get update
-apt-get install -y xvfb xauth
+sudo apt-get update
+sudo apt-get install -y xvfb xauth
 
 # Start the application with xvfb-run
 xvfb-run --auto-servernum --server-args='-screen 0 1024x768x24' npm run start
